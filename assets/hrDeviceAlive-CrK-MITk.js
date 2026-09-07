e6e384fff98c5dbc90fe8f8dd283dff261a67141
SynthYoga publish <synthyoga-bot@users.noreply.github.com>
2026-09-07 12:52:03 +1000

    Publish SynthYoga web 2026-09-07T0252Z

---
+++ assets/hrDeviceAlive-CrK-MITk.js
@@ -0,0 +1,1 @@
+function e(e){let t=e.deviceKind===`hr`||e.deviceKind===`altius`||e.deviceKind===`dafit`||e.deviceKind===`fitpro`,n=t?null:e.bandAddress,r=e.hrAddress||(t?e.bandAddress:null);return!!(n||r)}function t(e){return!!e.bikeAddress}function n(e){return e.bikeAutoCapture===!0&&t(e)}function r(t){return t.hrActivityAutoDetect===!0&&e(t)}export{e as i,t as n,r,n as t};
